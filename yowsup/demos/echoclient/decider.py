81677cef6041643ad5da2e6b5c00f8f06037a0df
Hermann Krumrey <hermann@krumreyh.com>
2015-11-28 21:40:05 +0100

    Implemented command line commands. Note: Don't enable man.

--- yowsup/demos/echoclient/decider.py
+++ yowsup/demos/echoclient/decider.py
@@ -2,17 +2,38 @@ def decide(messageProtocolEntity):
 
     sentmessage = messageProtocolEntity.getBody()
     sender = messageProtocolEntity.getFrom(False)
-    decision = ["",sender,""]
+    sendername = adressbook(sender)
+    decision = ["", sender, ""]
 
-    if "keks" in sentmessage.lower():
+    print("recv: " + sendername + ": " + sentmessage.lower())
+
+    if "keks" in sentmessage.lower() or "cookie" in sentmessage.lower():
         decision[0] = "Ich will auch Kekse!"
     elif "kuchen" in sentmessage.lower():
         decision[0] = "Ich mag Kuchen."
-    elif "rsync-backup" == sentmessage and sender == "4917628727937":
-        decision[2] = "rsync-backup-local"
     elif "wã¼rfel" in sentmessage.lower() or "wuerfel" in sentmessage.lower():
         decision[0] = "https://play.google.com/store/apps/details?id=com.namibsun.android.dice"
-    else:
-        print("illegal action " + sentmessage.lower())
+    elif "ð" in sentmessage.lower():
+        decision[0] = "ððð"
+
+    elif "term: " in sentmessage.lower() and sendername.split(" ")[0] == "Hermann":
+        decision[2] = sentmessage.lower().split("term: ")[1]
+    elif sentmessage.lower().startswith("term: "):
+        command = sentmessage.lower().split("term: ")[1]
+        if command.startswith("ls") \
+                or command.startswith("man"):
+            decision[2] = command
+        else: decision[0] = "Invalid command"
+
+
+    if decision[0]: print("sent: " + sendername + ": " + decision[0])
+    elif decision[2]: print("cmnd: " + decision[2])
 
     return decision
+
+
+def adressbook(adress):
+    if adress == "4915779781557-1418747022":    return "Land of the very Brave      "
+    elif adress == "4917628727937-1448730289":  return "Bottesting                  "
+    elif adress == "4917628727937":             return "Hermann                     "
+    else: return adress
--- yowsup/demos/echoclient/layer.py
+++ yowsup/demos/echoclient/layer.py
@@ -2,15 +2,21 @@ from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocol
 from yowsup.layers.interface import YowInterfaceLayer, ProtocolEntityCallback
 from yowsup.demos.echoclient.decider import decide
 import os
+import subprocess
 
 class EchoLayer(YowInterfaceLayer):
 
     @ProtocolEntityCallback("message")
     def onMessage(self, messageProtocolEntity):
 
+        if not messageProtocolEntity.getType() == 'text': return
+
         decision = decide(messageProtocolEntity)
 
         if decision[2]:
+            decision[0] = subprocess.check_output(args=decision[2], shell=True)
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(decision[0], to=messageProtocolEntity.getFrom())
+            self.toLower(outgoingMessageProtocolEntity)
             os.system(decision[2])
         elif decision[0]:
             outgoingMessageProtocolEntity = TextMessageProtocolEntity(decision[0], to=messageProtocolEntity.getFrom())
