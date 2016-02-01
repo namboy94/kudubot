6f57e93af910e7419bf20a15e5504be6ca7ebcf7
Hermann Krumrey <hermann@krumreyh.com>
2016-02-01 21:46:49 +0100

    Changed License to GPL3.

--- bot/plugins/internetServicePlugins/ImageSender.py
+++ bot/plugins/internetServicePlugins/ImageSender.py
@@ -1,13 +1,29 @@
 # coding=utf-8
+"""
+Copyright 2015,2016 Hermann Krumrey
 
+This file is part of whatsapp-bot.
+
+    whatsapp-bot makes use of various third-party python modules to serve
+    information via the online chat service Whatsapp.
+
+    whatsapp-bot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    whatsapp-bot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with whatsapp-bot.  If not, see <http://www.gnu.org/licenses/>.
 """
-Plugin that allows the sending of images from links
-@author Hermann Krumrey <hermann@krumreyh.com>
-"""
+
 import os
 import re
 from threading import Thread
-
 from yowsup.layers.protocol_messages.protocolentities import TextMessageProtocolEntity
 from plugins.GenericPlugin import GenericPlugin
 from subprocess import Popen, PIPE
--- bot/plugins/simpleTextResponses/SimpleContainsResponse.py
+++ bot/plugins/simpleTextResponses/SimpleContainsResponse.py
@@ -1,8 +1,24 @@
 # coding=utf-8
-
 """
-plugin that answers to strings containing specific substrings
-@author Hermann Krumrey <hermann@krumreyh.com>
+Copyright 2015,2016 Hermann Krumrey
+
+This file is part of whatsapp-bot.
+
+    whatsapp-bot makes use of various third-party python modules to serve
+    information via the online chat service Whatsapp.
+
+    whatsapp-bot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    whatsapp-bot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with whatsapp-bot.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import random
