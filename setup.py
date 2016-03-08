c863bb8e744634a3a554cac693eeac8595d1f0ec
Hermann Krumrey <hermann@krumreyh.com>
2016-03-08 02:08:38 +0100

    Fixed continuous script.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def readme():
 
 
 setup(name='whatsbot',
-      version='0.1',
+      version='0.1.1',
       description='An automated Whatsapp bot',
       long_description=readme(),
       classifiers=['Development Status :: 2 - Pre-Alpha',
