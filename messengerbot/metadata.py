781cf53586a7a79b7243e55fb6d3e1ba0d259331
Hermann Krumrey <hermann@krumreyh.com>
2016-05-06 19:41:48 +0200

    Version bump to 0.6.3.2 after integrating documentation

--- messengerbot/metadata.py
+++ messengerbot/metadata.py
@@ -36,7 +36,7 @@ project_description = "A bot that interfaces with several different messenger se
 A short description of the project
 """
 
-version_number = "0.6.3.1"
+version_number = "0.6.3.2"
 """
 The current version of the program.
 """
