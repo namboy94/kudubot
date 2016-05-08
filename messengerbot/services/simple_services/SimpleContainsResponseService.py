10ab23ab0a100ee1b1892a3ecd7a3febf445bf4c
Hermann Krumrey <hermann@krumreyh.com>
2016-05-08 18:55:50 +0200

    Added a couple of new simple responses.

--- messengerbot/services/simple_services/SimpleContainsResponseService.py
+++ messengerbot/services/simple_services/SimpleContainsResponseService.py
@@ -45,6 +45,7 @@ class SimpleContainsResponseService(Service):
     per say.
     """
 
+    # noinspection PyRedundantParentheses
     case_insensitive_options = {("keks", "cookie"): ["Ich will auch Kekse!",
                                                      "Wo gibt's Kekse?",
                                                      "Kekse sind klasse!",
@@ -60,7 +61,9 @@ class SimpleContainsResponseService(Service):
                                 ("beste bot", "bester bot"): ["😘"],
                                 ("doofer bot", "scheiß bot"): ["🖕🏻", "😡"],
                                 ("chicken", "nuggets", "huhn", "hühnchen"): ["🐤", "Die armen Kücken!\n🐤🐤🐤"],
-                                ("scheiße", "kacke"): ["💩"]}
+                                ("scheiße", "kacke"): ["💩"],
+                                ("kaputt", "zerbrochen"): ["¯\\_(ツ)_/¯"],
+                                ("😂", "😂"): ["😂😂😂"]}
     """
     Case-insensitive defined response conditions and responses
     """
