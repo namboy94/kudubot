0b97a410399b812f4d2374ac78d6192550477394
Hermann Krumrey <hermann@krumreyh.com>
2016-05-09 14:20:22 +0200

    Created Service skeleton for Random Key Generator Service.

---
+++ messengerbot/services/local_services/RandomKeyGeneratorService.py
@@ -0,0 +1,67 @@
+# coding=utf-8
+"""
+Copyright 2015,2016 Hermann Krumrey
+
+This file is part of messengerbot.
+
+    messengerbot makes use of various third-party python modules to serve
+    information via the online chat services.
+
+    messengerbot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    messengerbot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with messengerbot.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+# imports
+import re
+
+from messengerbot.servicehandlers.Service import Service
+from messengerbot.connection.generic.Message import Message
+
+
+class RandomKeyGeneratorService(Service):
+    """
+    The RandomKeyGeneratorService Class that extends the generic Service class.
+    The service sends a random key of specified length
+    """
+
+    identifier = "random_key_generator"
+    """
+    The identifier for this service
+    """
+
+    help_description = {"en": "No Help Description Available",
+                        "de": "Keine Hilfsbeschreibung verfügbar"}
+    """
+    Help description for this service.
+    """
+
+    def process_message(self, message: Message) -> None:
+        """
+        Process a message according to the service's functionality
+
+        :param message: the message to process
+        :return: None
+        """
+        reply = ""
+        reply_message = self.generate_reply_message(message, "Random Key", reply)
+        self.send_text_message(reply_message)
+
+    @staticmethod
+    def regex_check(message: Message) -> bool:
+        """
+        Checks if the user input is valid for this service to continue
+
+        :return: True if input is valid, False otherwise
+        """
+        regex = ""
+        return re.search(re.compile(regex), message.message_body.lower())
