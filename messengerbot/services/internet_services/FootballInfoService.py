613d8122b03b7d3f484bc2f5a8e88332c52342be
Hermann Krumrey <hermann@krumreyh.com>
2016-05-11 10:41:16 +0200

    Implemented text image sending in relevant services

--- messengerbot/services/internet_services/FootballInfoService.py
+++ messengerbot/services/internet_services/FootballInfoService.py
@@ -104,7 +104,11 @@ class FootballInfoService(Service):
             reply = self.get_matchday_info()
 
         reply_message = self.generate_reply_message(message, "Football Info", reply)
-        self.send_text_message(reply_message)
+
+        if self.connection.identifier in ["whatsapp", "telegram"]:
+            self.send_text_as_image_message(reply_message)
+        else:
+            self.send_text_message(reply_message)
 
     @staticmethod
     def regex_check(message: Message) -> bool:
@@ -225,39 +229,23 @@ class FootballInfoService(Service):
             if team_name_length > longest_team_name:
                 longest_team_name = team_name_length
 
-        if self.connection.identifier in ["whatsapp", "telegram"]:  # For mobile messengers
+        formatted_string = "#    " + "Team Name".ljust(longest_team_name) + \
+                           "  P     W     D     L     GF    GA    GD    Pts\n"
+        divider = "  "
 
-            formatted_string = "#   " + "Team Name".ljust(12) + " FOR:AG  Pts"
-            divider = " "
+        for position in range(1, len(league_table_dictionary) + 1):
+            line = "\n" + str(position).ljust(3) + divider
+            line += league_table_dictionary[position]["team_name"].ljust(longest_team_name) + divider
+            line += league_table_dictionary[position]["matches"].ljust(4) + divider
+            line += league_table_dictionary[position]["wins"].ljust(4) + divider
+            line += league_table_dictionary[position]["draws"].ljust(4) + divider
+            line += league_table_dictionary[position]["losses"].ljust(4) + divider
+            line += league_table_dictionary[position]["goals_for"].ljust(4) + divider
+            line += league_table_dictionary[position]["goals_against"].ljust(4) + divider
+            line += league_table_dictionary[position]["goal_difference"].rjust(4) + divider
+            line += league_table_dictionary[position]["points"].ljust(4)
 
-            for position in range(1, len(league_table_dictionary) + 1):
-                line = "\n" + str(position).ljust(3) + divider
-                line += league_table_dictionary[position]["team_name"].ljust(longest_team_name)[0:15] + divider
-                line += league_table_dictionary[position]["goals_for"] + ":"
-                line += league_table_dictionary[position]["goals_against"] + divider
-                line += league_table_dictionary[position]["points"]
-
-                formatted_string += line
-
-        else:  # For normal messages like email
-
-            formatted_string = "#    " + "Team Name".ljust(longest_team_name) + \
-                               "  P     W     D     L     GF    GA    GD    Pts\n"
-            divider = "  "
-
-            for position in range(1, len(league_table_dictionary) + 1):
-                line = "\n" + str(position).ljust(3) + divider
-                line += league_table_dictionary[position]["team_name"].ljust(longest_team_name) + divider
-                line += league_table_dictionary[position]["matches"].ljust(4) + divider
-                line += league_table_dictionary[position]["wins"].ljust(4) + divider
-                line += league_table_dictionary[position]["draws"].ljust(4) + divider
-                line += league_table_dictionary[position]["losses"].ljust(4) + divider
-                line += league_table_dictionary[position]["goals_for"].ljust(4) + divider
-                line += league_table_dictionary[position]["goals_against"].ljust(4) + divider
-                line += league_table_dictionary[position]["goal_difference"].rjust(4) + divider
-                line += league_table_dictionary[position]["points"].ljust(4)
-
-                formatted_string += line
+            formatted_string += line
 
         return formatted_string
 
