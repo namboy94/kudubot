881da2b506c38098ec17f02a3e9be2f33043e84f
Hermann Krumrey <hermann@krumreyh.com>
2016-08-05 12:10:02 +0200

    Version bump to 0.7.6

--- kudubot/metadata.py
+++ kudubot/metadata.py
@@ -43,7 +43,7 @@ project_description = "A bot that interfaces with several different messenger se
 A short description of the project
 """
 
-version_number = "0.7.5"
+version_number = "0.7.6"
 """
 The current version of the program.
 """
