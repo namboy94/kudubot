9a9ee7a758a3a0de3b0714b0cf9970698bd00514
Hermann Krumrey <hermann@krumreyh.com>
2017-04-24 20:19:18 +0000

    Version bump to 0.12.0

--- kudubot/metadata.py
+++ kudubot/metadata.py
@@ -27,7 +27,7 @@ The metadata is stored here. It can be used by any other module in this project 
 notably by the setup.py file
 """
 
-version = "0.11.0"  # pragma: no cover
+version = "0.12.0"  # pragma: no cover
 """
 The current version of the program
 """
