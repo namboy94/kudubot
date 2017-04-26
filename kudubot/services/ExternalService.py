88c42ffcab2a3c5b92cbc14c16a5387daea5a8d1
Hermann Krumrey <hermann@krumreyh.com>
2017-04-26 13:25:56 +0000

    Start implementing Hello Rust Service

--- kudubot/services/ExternalService.py
+++ kudubot/services/ExternalService.py
@@ -1,8 +1,33 @@
+"""
+LICENSE:
+Copyright 2015-2017 Hermann Krumrey
+
+This file is part of kudubot.
+
+    kudubot is a chat bot framework. It allows developers to write
+    services for arbitrary chat services.
+
+    kudubot is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    kudubot is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with kudubot.  If not, see <http://www.gnu.org/licenses/>.
+LICENSE
+"""
+
 import os
 import json
 import time
 import requests
 from subprocess import Popen
+from bs4 import BeautifulSoup
 from typing import List, Tuple, Dict
 from kudubot.services.Service import Service
 from kudubot.entities.Message import Message, from_dict as message_from_dict
@@ -170,3 +195,26 @@ class ExternalService(Service):
             os.remove(message_file)
         if os.path.isfile(response_file):
             os.remove(response_file)
+
+    # noinspection PyMethodMayBeStatic
+    def resolve_github_release_asset_url(self, owner: str, repository: str, filename: str) -> str:
+        """
+        Generates a download URL for a Github release artifact/asset
+
+        :param owner: The owner of the Github repository
+        :param repository: The name of the repository
+        :param filename: The file to search for
+        :return: The URL to the file, if it was found. If not, an empty string is returned
+        """
+
+        release_page = "https://github.com/" + owner + "/" + repository + "/releases/latest"
+        soup = BeautifulSoup(requests.get(release_page).text, "html.parser")
+        downloads = soup.select(".release-downloads")[0]
+
+        for download in downloads.select("a"):
+            name = download.select("strong")[0]
+
+            if name == filename:
+                return "https://github.com" + download["href"]
+
+        return ""
