6be97da9d5d2003177aeeb737cc264f51c5fdd25
Hermann Krumrey <hermann@krumreyh.com>
2017-04-28 09:19:20 +0000

    Fix import error

--- kudubot/services/native/anime_reminder/AnimeReminderService.py
+++ kudubot/services/native/anime_reminder/AnimeReminderService.py
@@ -27,10 +27,9 @@ import re
 import time
 from typing import List, Dict
 
-from kudubot.services.anime_reminder.scraper import scrape_reddit_discussion_threads
-
 from kudubot.entities.Message import Message
 from kudubot.services.HelperService import HelperService
+from kudubot.services.native.anime_reminder.scraper import scrape_reddit_discussion_threads
 from kudubot.services.native.anime_reminder.database import initialize_database, store_subscription, \
     delete_subscription, get_subscriptions, thread_exists, store_thread
 
