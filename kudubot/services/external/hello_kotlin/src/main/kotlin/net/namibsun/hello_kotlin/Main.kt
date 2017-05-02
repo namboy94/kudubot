f2471d134babf6e8bdeae209183085a78008ec48
Hermann Krumrey <hermann@krumreyh.com>
2017-05-02 09:07:22 +0000

    Start implementing weather service in Kotlin

--- kudubot/services/external/hello_kotlin/src/main/kotlin/net/namibsun/hello_kotlin/Main.kt
+++ kudubot/services/external/hello_kotlin/src/main/kotlin/net/namibsun/hello_kotlin/Main.kt
@@ -2,6 +2,9 @@ package net.namibsun.hello_kotlin
 
 import net.namibsun.kudubot_bindings.KudubotCommunicationHandler
 import net.namibsun.kudubot_bindings.Modes
+import com.github.fedy2.weather.YahooWeatherService
+import com.github.fedy2.weather.data.Channel
+import com.github.fedy2.weather.data.unit.DegreeUnit
 
 
 /**
@@ -11,6 +14,7 @@ import net.namibsun.kudubot_bindings.Modes
  */
 fun main(args: Array<String>) {
 
+    /*
     val communicator = KudubotCommunicationHandler(args)
 
     when (communicator.mode) {
@@ -19,5 +23,15 @@ fun main(args: Array<String>) {
                 communicator.incomingMessage.messageBody.toLowerCase() == "hello kotlin!")
         Modes.HANDLE_MESSAGE -> communicator.reply("Hello Kotlin", "Hi!")
     }
+    */
 
+    val weather = YahooWeatherService()
+    val results = weather.getForecastForLocation("Windhoek", DegreeUnit.CELSIUS).all()
+
+    println(results[0].image)
+    println(results[0].units)
+    println(results[0].astronomy)
+    println(results[0].ttl)
+    println(results[0].wind)
+    println(results[0].item.condition)
 }
