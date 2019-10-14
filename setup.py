5494371de4f0d83f64503283a504ad1c0616d8bb
Hermann Krumrey <hermann@krumreyh.com>
2019-10-14 08:57:10 +0200

    Integrate puffotter cli_start

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ if __name__ == "__main__":
             "typing",
             "bokkichat",
             "sqlalchemy",
-            "sentry-sdk"
+            "sentry-sdk",
+            "puffotter"
         ],
         include_package_data=True,
         zip_safe=False
